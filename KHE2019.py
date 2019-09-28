ef26a6639657b4ae3524e980f62791dae3e274be
emilyCrobinson <46360123+emilyCrobinson@users.noreply.github.com>
2019-09-28 11:25:35 -0400

    Updating landing for dynamic forms.

--- KHE2019.py
+++ KHE2019.py
@@ -1,10 +1,16 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def main():
-    return render_template('index.html')
+    if request.method == 'GET':
+        return render_template('index.html')
+    elif request.method == 'POST':
+        values = request.form.getlist("OnOffSwitch")
+        for item in values:
+            print(item)
+        return render_template('index.html')
 
 if __name__ == '__main__':
     app.run()
