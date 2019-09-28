6e39b71224679b68a3aef12078badc2523bdeaef
Mike Wolfe <miketechgame@gmail.com>
2019-09-28 18:46:13 +0000

    Added jinja and zones for outlet addition.

--- KHE2019.py
+++ KHE2019.py
@@ -14,13 +14,18 @@ Routes to set up zones and outlets
 @app.route("/add-off", methods=['GET', 'POST'])
 def add_off():
     if request.method == "GET":
-        Response = make_response(render_template("add_off.html"))
+        Zones = storage.GetZones()
+
+        Response = make_response(render_template("add_off.html", Zones=Zones))
         return Response
     elif request.method == "POST":
+        OutletName = request.form.get("outletname")
+        OffSelect = request.form.get("addoff")
+
         Chatter = OutletChatter()
         OffId = Chatter.GetOffId()
         
-        storage.AddOffId(OffId)
+        storage.AddOffId(OffSelect, OffId, OutletName)
 
         Response = make_response(redirect("/add-off"))
         return Response
@@ -28,13 +33,18 @@ def add_off():
 @app.route("/add-on", methods=['GET', 'POST'])
 def add_on():
     if request.method == "GET":
-        Response = make_response(render_template("add_on.html"))
+        Zones = storage.GetZones()
+
+        Response = make_response(render_template("add_on.html", Zones=Zones))
         return Response
     elif request.method == "POST":
+        OutletName = request.form.get("outletname")
+        OnSelect = request.form.get("addon")
+
         Chatter = OutletChatter()
         OnId = Chatter.GetOnId()
         
-        storage.AddOnId(OnId)
+        storage.AddOnId(OnSelect, OnId, OutletName)
 
         Response = make_response(redirect("/add-off"))
         return Response
